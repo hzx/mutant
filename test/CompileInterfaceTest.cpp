800f3d2c30eeb380343b66cf34fb6ae1f75fb921
Dmitry Dementev <dem@oldschooldev.com>
2016-12-22 03:52:34 +0300

    add tests CompileInterfaceTest

--- test/CompileInterfaceTest.cpp
+++ test/CompileInterfaceTest.cpp
@@ -4,3 +4,19 @@
 class CompileInterfaceTest: public ::testing::Test {
 public:
 };
+
+
+TEST_F(CompileInterfaceTest, interface) {
+}
+
+
+TEST_F(CompileInterfaceTest, extendsSingle) {
+}
+
+
+TEST_F(CompileInterfaceTest, extendsMultiple) {
+}
+
+
+TEST_F(CompileInterfaceTest, errorMethodRedeclaration) {
+}
