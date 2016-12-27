ee1a2409f9eec56ae2963f3b09ecb501e0dc6081
Dmitry Dementev <dem@oldschooldev.com>
2016-12-27 03:58:35 +0300

    add Compiler::compile TODO

--- mutant/Compiler.cpp
+++ mutant/Compiler.cpp
@@ -4,5 +4,16 @@
 
 
 int Compiler::compile(Build& build, Project& project) {
+  // TODO: think about incremental build - reload only changed files,
+  // tokenize and parse and analyze, then analyze depended code.
+  // Think about diff detection, dependency organization (maybe store
+  // separately).
+
+  // loader
+  // lexer
+  // parser
+  // analyzer
+  // formatter
+
   return ERROR_OK;
 }
