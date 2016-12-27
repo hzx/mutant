b79c1c72d81e5fc2a472d2e98456a9e3e7d2016d
Dmitry Dementev <dem@oldschooldev.com>
2016-12-27 04:08:38 +0300

    add dummy ObjSerializer

---
+++ mutant/ObjSerializer.cpp
@@ -0,0 +1,12 @@
+#include "ObjSerializer.h"
+
+#include "error.h"
+
+
+int ObjSerializer::serialize() {
+  return ERROR_OK;
+}
+
+int ObjSerializer::deserialize() {
+  return ERROR_OK;
+}
---
+++ mutant/ObjSerializer.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+class ObjSerializer {
+public:
+  int serialize();
+  int deserialize();
+};
