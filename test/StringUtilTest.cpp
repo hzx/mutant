d36d95f6922e410f1c5fad6ca97a427cc055acfb
Dmitry Dementev <dem@oldschooldev.com>
2017-01-09 02:42:46 +0300

    add StringUtilTest

---
+++ test/StringUtilTest.cpp
@@ -0,0 +1,50 @@
+#include <gmock/gmock.h>
+
+#include <string>
+#include <vector>
+
+#include "mutant/StringUtil.h"
+
+
+TEST(StringUtilTest, find) {
+  std::string content = " a  ! ' \" ";
+
+  size_t pos = StringUtil::find(content, 0, content.size(), [](char c) {
+    return c == '!';
+  });
+
+  ASSERT_EQ(4, pos);
+
+  pos = StringUtil::find(content, 0, content.size(), [](char c) {
+    return c == 'b';
+  });
+
+  ASSERT_EQ(content.size(), pos);
+}
+
+
+TEST(StringUtilTest, extract) {
+  std::string content = "  'foo/path here' ";
+  std::string content2 = " \"Some value\"";
+
+  std::string expected = "foo/path here";
+  std::string expected2 = "Some value";
+
+  std::string actual = StringUtil::extract(content, 0, content.size());
+  std::string actual2 = StringUtil::extract(content2, 0, content2.size());
+
+  ASSERT_EQ(expected, actual);
+  ASSERT_EQ(expected2, actual2);
+}
+
+
+TEST(StringUtilTest, parseArgs) {
+  std::string content = " -I foo/path ";
+  std::vector<std::string> expected = {
+    "-I", "foo/path"
+  };
+
+  std::vector<std::string> actual = StringUtil::parseArgs(content, 0, content.size());
+
+  ASSERT_EQ(expected, actual);
+}
