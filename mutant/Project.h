84ae129dbc9dfae6e8c4c2e356005dd762c69fa7
Dmitry Dementev <dem@oldschooldev.com>
2017-01-09 02:20:12 +0300

    add Project::targets

--- mutant/Project.h
+++ mutant/Project.h
@@ -2,10 +2,12 @@
 
 #include <string>
 #include <vector>
+#include <map>
 
 
 class Project {
 public:
   std::string path;
   std::vector<std::string> repositories;
+  std::map<std::string, std::string> targets;
 };
